59536a269d5b9c0c0d28437b82c6e05320f57ab4
Jonathan Garbee <jonathan@garbee.me>
2012-07-30 16:15:42 -0400

    Contact page added
    Added the contact page with a very basic and *insecure* form.  It should
    not really be used in a live site.

--- pages/about/page_about.php
+++ pages/about/page_about.php
@@ -11,3 +11,5 @@
 <p>Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. </p>
 
 </div>
+
+<div  class="span12"></div> <!--This div just adds some spacing in between the content and footer so things don't look so smushed together.-->
